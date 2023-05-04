cb7115200e70d8f0a300d1ce6691d05867123843
Gonny <runic1541@gmail.com>
2023-05-04 21:40:35 +0900

    Add a Gameplay Abilities Plugin

--- Source/ActionRPG/ActionRPG.Build.cs
+++ Source/ActionRPG/ActionRPG.Build.cs
@@ -8,7 +8,8 @@ public class ActionRPG : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore",
+			"HeadMountedDisplay", "EnhancedInput", "GameplayTasks", "GameplayAbilities", "GameplayTags" });
 
         PublicIncludePaths.Add("ActionRPG");
     }
