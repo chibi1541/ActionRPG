f0daa61ede543b1caba9a12c49435933283f0c6d
Chibi <runic1541@gmail.com>
2023-12-19 23:47:26 +0900

    UISystem #1 PrimaryGameLayout [Working] Add a GameUIManagerSubsystem

---
+++ Source/ActionRPG/Subsystem/UserInterface/ARGameUIManagerSubsystem.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ActionRPG_Lib.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+
+#include "ARGameUIManagerSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONRPG_API UARGameUIManagerSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+
+};
