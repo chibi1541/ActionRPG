0efa0582c2aa68b2b3195f94cecba02f2c641748
Chibi <runic1541@gmail.com>
2023-12-26 22:57:04 +0900

    UISystem #1-3 PrimaryGameLayout [Warking] Add HUDLayout's DataAsset

---
+++ Source/ActionRPG/UserInterface/ARHUDLayoutSet.h
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ActionRPG_Lib.h"
+#include "Engine/DataAsset.h"
+#include "CommonActivatableWidget.h"
+#include "GameplayTagContainer.h"
+
+#include "ARHUDLayoutSet.generated.h"
+
+
+USTRUCT()
+struct FHUDLayoutInfo
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY( EditAnywhere, Category = UI )
+		TSoftClassPtr<UCommonActivatableWidget> LayoutClass;
+
+	UPROPERTY( EditAnywhere, Category = UI, meta = ( Categories = "UI.Layer" ) )
+		FGameplayTag LayerTag;
+};
+
+USTRUCT()
+struct FHUDElementInfo
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY( EditAnywhere, Category = UI )
+		TSoftClassPtr<UUserWidget> WidgetClass;
+
+	UPROPERTY( EditAnywhere, Category = UI )
+		FGameplayTag SlotTag;
+};
+
+UCLASS( BlueprintType )
+class ACTIONRPG_API UARHUDLayoutSet : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UARHUDLayoutSet();
+
+	void AddLayout( AActor* Actor );
+
+private:
+
+	UPROPERTY( EditAnywhere, Category = UI )
+		TArray<FHUDLayoutInfo> Layout;
+
+	UPROPERTY( EditAnywhere, Category = UI )
+		TArray<FHUDElementInfo> Widgets;
+
+};
